bc76119bdf5bf830338a894d57a6fb93e4c1ec63
Stefano Carelli <globalnetworkstep@gmail.com>
2026-08-02 09:49:51 +0200

    Aggiunta versione modificata

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-let v = -10;
+let v = 29;
 
-// 1. Controllo di validità: se v è minore di 0 OPPURE maggiore di 30
+console.log("--- SOLUZIONE CON IF / ELSE ---");
+
+// Sfruttiamo il funzionamento "a cascata" degli if/else
 if (v < 0 || v > 30) {
     console.log("voto non valido");
-} 
-// 2. Se passiamo il primo blocco, sappiamo con certezza che il voto è tra 0 e 30
-else if (v < 18) {
+} else if (v < 18) {
     console.log("insufficiente");
-} 
-else if (v >= 18 && v < 21) {
+} else if (v < 21) {
     console.log("sufficiente");
-} 
-else if (v >= 21 && v < 24) {
+} else if (v < 24) {
     console.log("buono");
-} 
-else if (v >= 24 && v < 27) {
+} else if (v < 27) {
     console.log("distinto");
-} 
-else if (v >= 27 && v <= 29) {
+} else if (v <= 29) {
     console.log("ottimo");
-} 
-else if (v === 30) {
+} else if (v === 30) {
     console.log("eccellente");
+}
+
+
+console.log("--- SOLUZIONE CON SWITCH ---");
+
+// Usiamo switch(true) che è il metodo classico per usare 
+// operatori di disuguaglianza (<, >) dentro i "case"
+switch (true) {
+    case (v < 0 || v > 30):
+        console.log("voto non valido");
+        break;
+    case (v < 18):
+        console.log("insufficiente");
+        break;
+    case (v >= 18 && v < 21):
+        console.log("sufficiente");
+        break;
+    case (v >= 21 && v < 24):
+        console.log("buono");
+        break;
+    case (v >= 24 && v < 27):
+        console.log("distinto");
+        break;
+    case (v >= 27 && v <= 29):
+        console.log("ottimo");
+        break;
+    case (v === 30):
+        console.log("eccellente");
+        break;
 }
